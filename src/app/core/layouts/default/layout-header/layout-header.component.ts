5d9cb7e48f85f86da8734a767450503389a4c650
Samson Davidoff <samsondavidoff@gmail.com>
2022-08-25 03:19:48 +0430

    Change header elements when fixed sidebar hides

--- src/app/core/layouts/default/layout-header/layout-header.component.ts
+++ src/app/core/layouts/default/layout-header/layout-header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject, takeUntil } from "rxjs";
+import { select, Store } from "@ngrx/store";
+import { RootState } from "@core/store/root.reducer";
+import { selectFixedSidebarState } from "@core/store/root.selectors";
 
 @Component({
   selector: 'instello-layout-header',
   templateUrl: './layout-header.component.html',
   styleUrls: ['./layout-header.component.scss']
 })
-export class LayoutHeaderComponent implements OnInit {
+export class LayoutHeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  destroy$ = new Subject<void>();
+  showFixedSidebarObservable$!: Observable<boolean>;
+  fixedSidebarVisible = true;
 
-  ngOnInit(): void {
+  constructor(private store: Store<{ root: RootState }>) {
+    this.showFixedSidebarObservable$ = store.pipe(select(selectFixedSidebarState));
   }
 
+  ngOnInit() {
+    this.showFixedSidebarObservable$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(fixedSidebarVisible => this.fixedSidebarVisible = fixedSidebarVisible);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
